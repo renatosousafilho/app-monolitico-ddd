b273f6a57a67347477087be55816a1897a728699
Renato Filho <renatosousafilho@gmail.com>
2024-06-04 18:41:43 -0300

    Sistemas Monolíticos: cria entidade Product em store-catalog

---
+++ src/modules/store-catalog/domain/entity/Product.ts
@@ -0,0 +1,28 @@
+import AggregateRoot from '../../../@shared/domain/entity/aggregate-root.interface';
+import BaseEntity from '../../../@shared/domain/entity/base.entity';
+import Id from '../../../@shared/domain/value-object/id.value-object';
+
+export default class Product extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _description: string;
+  private _salesPrice: number;
+  
+  constructor(id: Id, name: string, description: string, salesPrice: number) {
+    super(id);
+    this._name = name;
+    this._description = description;
+    this._salesPrice = salesPrice;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get salesPrice(): number {
+    return this._salesPrice;
+  }
+}

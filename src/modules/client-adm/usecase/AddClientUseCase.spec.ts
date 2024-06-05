d8cd3537c0bef9f96315cd016fe6a242e6ce5c6e
Renato Filho <renatosousafilho@gmail.com>
2024-06-05 13:31:31 -0300

    Sistemas Monolíticos: Criando AddClientUseCase com teste

---
+++ src/modules/client-adm/usecase/AddClientUseCase.spec.ts
@@ -0,0 +1,34 @@
+import Client from '../domain/Client';
+import ClientGateway from '../gateway/ClientGateway';
+import AddClientUseCase from './AddClientUseCase';
+
+class ClientRepositoryMock implements ClientGateway {
+  async add(client: Client): Promise<Client> {
+    return Promise.resolve(client);
+  }
+}
+
+describe('AddClientUseCase', () => {
+  it('should add a client', async () => {
+    // Arrange
+    const clientRepository = new ClientRepositoryMock();
+    const addClientUseCase = new AddClientUseCase(clientRepository);
+    const input = {
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      document: '12345678900',
+      address: '123 Main St'
+    };
+
+    // Act
+    const client = await addClientUseCase.execute(input);
+
+    // Assert
+    expect(client).toBeDefined();
+    expect(client.id).toBeDefined();
+    expect(client.name).toBe('John Doe');
+    expect(client.email).toBe('johndoe@mail.com');
+    expect(client.document).toBe('12345678900');
+    expect(client.address).toBe('123 Main St');
+  });
+});
---
+++ src/modules/client-adm/usecase/AddClientUseCase.ts
@@ -0,0 +1,51 @@
+import Id from '../../@shared/domain/value-object/id.value-object';
+import Client from '../domain/Client';
+import ClientGateway from '../gateway/ClientGateway';
+
+type AddClientUseCaseInput = { 
+  id?: string;
+  name: string;
+  email: string;
+  document: string;
+  address: string;
+};
+
+type AddClientUseCaseOutput = {
+  id: string;
+  name: string;
+  email: string;
+  document: string;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export default class AddClientUseCase {
+  private _clientRepository: ClientGateway;
+
+  constructor(clientRepository: ClientGateway) {
+    this._clientRepository = clientRepository;
+  }
+
+  async execute(input: AddClientUseCaseInput): Promise<AddClientUseCaseOutput> {
+    const client = new Client({
+      id: new Id(input.id),
+      name: input.name,
+      email: input.email,
+      document: input.document,
+      address: input.address
+    });
+
+    await this._clientRepository.add(client);
+
+    return {
+      id: client.id.value,
+      name: client.name,
+      email: client.email,
+      document: client.document,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt
+    };
+  }
+}

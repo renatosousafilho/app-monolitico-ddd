54708f43fbabcb0dc0181a4f692d1c1790453cbd
Renato Filho <renatosousafilho@gmail.com>
2024-06-06 09:15:17 -0300

    Sistemas Monolíticos: Cria PaymentFacadeFactory

---
+++ src/modules/payment/facade/PaymentFacadeFactory.ts
@@ -0,0 +1,11 @@
+import TransactionRepository from '../repository/TransactionRepository';
+import ProcessPaymentUseCase from '../usecase/ProcessPaymentUseCase';
+import PaymentFacade from './PaymentFacade';
+
+export default class PaymentFacadeFactory {
+  static create() {
+    const repository = new TransactionRepository();
+    const usecase = new ProcessPaymentUseCase(repository);
+    return new PaymentFacade(usecase);
+  }
+}

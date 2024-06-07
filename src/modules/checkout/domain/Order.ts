12a19601ef5aaa9fc2d7a6c588d573112b0fb4cc
Renato Filho <renatosousafilho@gmail.com>
2024-06-07 11:24:58 -0300

    Sistemas Monolíticos: Cria as entidades Order e Product

---
+++ src/modules/checkout/domain/Order.ts
@@ -0,0 +1,46 @@
+import AggregateRoot from '../../@shared/domain/entity/aggregate-root.interface';
+import BaseEntity from '../../@shared/domain/entity/base.entity';
+import Id from '../../@shared/domain/value-object/id.value-object';
+import Client from './Client';
+import Product from './Product';
+
+type OrderProps = {
+  id?: Id;
+  client: Client;
+  products: Product[];
+  status?: string;
+}
+
+export default class Order extends BaseEntity implements AggregateRoot {
+  private _client: Client;
+  private _products: Product[];
+  private _status: string;
+
+  constructor(props: OrderProps) {
+    super(props.id);
+    this._client = props.client;
+    this._products = props.products;
+    this._status = props.status;
+  }
+
+  approve() {
+    this._status = 'approved';
+  }
+
+  get client() {
+    return this._client;
+  }
+
+  get products() {
+    return this._products;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+
+  get total() {
+    return this._products.reduce((acc, product) => acc + product.salesPrice, 0);
+  }
+}
